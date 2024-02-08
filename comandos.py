86e72c856393df42a83a89f198388250b32f77c6
Lucas Ponce <150847602+LucasPonc@users.noreply.github.com>
2024-02-08 18:06:29 +0000

    atualização

--- comandos.py
+++ comandos.py
@@ -7,6 +7,16 @@
 #clear comando para limpar o terminal
 #Ao digitar o comando python3 nomedoarquivo.py mostra o teste do arquivo 
 #Variaves:
+#esponenciação (1**2)
+#divisao (1/2)
+#inteiro da divisao (2//5) vai pegar só a parte inteira da divisão = 2
+#resto da divisão (5%2) vai retornar 1 que o resto da divisão
+#atribuição ou seja recebendo nome = a 
+#type descobre o tipo da variavel(var)
+#float numeros reais 2.5 ou 2,5
+
+
+
 
 #int - inteiro
 #print - imprimir 
