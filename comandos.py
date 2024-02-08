4c10b7133998f815a242c617e04ce5b079dae88e
Lucas Ponce <150847602+LucasPonc@users.noreply.github.com>
2024-02-08 12:22:55 +0000

    atualizar comandos

---
+++ comandos.py
@@ -0,0 +1,14 @@
+
+#procedimento para salvar os arquivos no github:
+
+#git add nome do arquivo.py
+#git commit -m "digitar texto para atualizacao"
+#git push
+#clear comando para limpar o terminal
+#Ao digitar o comando python3 nomedoarquivo.py mostra o teste do arquivo 
+#Variaves:
+
+#int - inteiro
+#print - imprimir 
+
+
