5c0444e4e378bf22f9912ed2f6d6961ecf31f37a
Lucas Ponce <150847602+LucasPonc@users.noreply.github.com>
2024-02-09 23:25:21 +0000

    até o exercicio 11

--- lista1.py
+++ lista1.py
@@ -1,247 +1,97 @@
 
 #print("---------------------------------exe 1---------------------------------------------")
-
 #1. Faça um programa que imprima o seu nome.
-
 #print("\n")
-
-
 #nome = str(input ('DIGITE SEU NOME: '))
-
-#print("\n")
-
 #print(nome)
 
-
-#print("\n")
-
-
 #print("-------------------------------exe 2-----------------------------------------------")
-
-
 #2. Faça um programa que imprima o produto dos valores 30 e 27.
-
 #print("\n")
-
 #valor1 = int(input ('DIGITE O PRIMEIRO NUMERO: '))
 #valor2 = int(input ('DIGITE O SEGUNDO NUMERO: '))
-
-#print("\n")
-
 #produto = (valor1 * valor2)
-
-#print("\n")
-
 #print(f'O valor do produto é: {produto}')
-
 #print("\n")
-
 #print("--------------------------------exe 3------------------------------------------")
 
-
 #3. Faça um programa que imprima a média aritmética entre os números 5, 8, 12.
-
 #print("\n")
-
 #valor1 = int(input ('DIGITE O PRIMEIRO NUMERO: '))
 #valor2 = int(input ('DIGITE O SEGUNDO NUMERO: '))
 #valor3 = int(input ('DIGITE O TERCEIRO NUMERO: '))
-
-
-
-
 #media = ((valor1 + valor2 + valor3)/3)
-
 #print("\n")
-
-
-
 #print(f'a media entre os 3 numeros é: {media}')
-
-
 #print("\n")
-
-
 
 #print("----------------------------------------exe 4--------------------------------------")
-
 #4. Faça um programa que leia e imprima um número inteiro.
-
-
-
 #numero = int(input ('DIGITE UM NUMERO INTEIRO: '))
-
-
 #print(f'o numero inteiro digitado é: {numero}')
 
-
-
-
-
-
-
-
-
-
-
-
 #print("------------------------------------exe 5------------------------------------------")
-
 #5. Faça um programa que leia dois números reais e os imprima.
 #num1 = input('digite num1: ')
 #num2 = input('digite num2: ')
 #print(num1)
 #print(num2)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #print("-------------------------------------------exe 6-----------------------------------")
-
 #6. Faça um programa que leia um número inteiro e imprima o seu
-#   antecessor e o seu sucessor.
-
-
-
-
-
-
-
-
-
-
-
-
-
+#antecessor e o seu sucessor.
+#print("\n")
+#num = int(input('digite um numero:'))
+#num_suc = num + 1
+#num_ant = num - 1
+#print('o numero informado foi {} seu sucessor e {} e seu antecessor e {}'.format(num,num_suc,num_ant))
+#print("\n")
 
 #print("--------------------------------------exe 7----------------------------------------")
-
 #7. Faça um programa que leia o nome o endereço e o telefone de
-#   um cliente e ao final, imprima esses dados.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#print("--------------------------------------exe 8----------------------------------------")
-
-#nome = input(str('digite seu nome:  '))
-#endereço = input(str('digite seu endereço:  '))
-#telefone = input(str('digite seu telefone:  '))
+#um cliente e ao final, imprima esses dados.
+#print("\n")
+#nome = input(str('digite seu nome:' ))
+#endereço = input(str('digite seu endereço:'))
+#telefone = input(str('digite seu nome:'))
 #print(nome)
 #print(endereço)
 #print(telefone)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#print("\n")
 
 #print("--------------------------------------exe 9----------------------------------------")
-
 #8. Faça um programa que leia dois números inteiros e imprima a
-#   subtração deles.
-
-
-#num1 = input(int('digite num1'))
-#num2 = input(int('digite num2'))
-#soma = num1 + num2
-#print(num1 + num2)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#subtração deles.
+#print("\n")
+#numero1 = int(input('digite numero1: '))
+#numero2 = int(input('digite numero2: '))
+#print(+numero1 - numero2)
+#print("\n")
 
   #print("--------------------------------------exe 10----------------------------------------")
-
-
 #9. Faça um programa que leia um número real e imprima ¼ deste número.
-
-
-
-
-
-
-
-
-
-
-
-
   #print("--------------------------------------exe 10----------------------------------------")
-
 #10. Faça um programa que leia três números reais e calcule a
 #    média aritmética destes números. Ao final, o programa deve
 #    imprimir o resultado do cálculo.
-
-
-
-
-
-
-
-
-
-
-
-
+#print("\n")
+#numero1 = float(input('digite num1: '))
+#numero2 = float(input('digite num2: '))
+#numero3 = float(input('digite num3: '))
+#print(numero1 + numero2 + numero3)/3
+#print("\n")
 
   #print("--------------------------------------exe 10----------------------------------------")
-
 #11. Faça um programa que leia dois números reais e calcule as
 #    quatro operações básicas entre estes dois números, adição,
 #    subtração,multiplicação e divisão. Ao final, o programa
-#    deve imprimir os resultados dos cálculos.
+#    deve imprimir os resultados dos cálculos
 
-
+numero1 = float(input('digite num1: '))
+numero2 = float(input('digite num2: '))
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
 
 
 
@@ -252,112 +102,37 @@
 
 
   #print("--------------------------------------exe 10----------------------------------------")
-
 #12. Faça um programa que leia um número real e calcule o
 #    quadrado deste número. Ao final, o programa deve
 #    imprimir o resultado do cálculo.
 
-
-
-
-
-
-
-
-
-
-
-
   #print("--------------------------------------exe 10----------------------------------------")
-
 #13. Faça um programa que leia o saldo de uma conta poupança e
 #    imprima o novo saldo, considerando um reajuste de 2%.
 
-
-
-
-
-
-
-
-
-
-
-
-
   #print("--------------------------------------exe 10----------------------------------------")
-
-
 #14. Faça um programa que leia a base e a altura de um retângulo
 #    e imprima o perímetro (base + altura) e a área (base * altura).
 
-
-
-
-
-
-
-
-
-
   #print("--------------------------------------exe 10----------------------------------------")
-
 #15. Faça um programa que leia o valor de um produto, o percentual
 #    do desconto desejado e imprima o valor do desconto e o valor
 #    do produto subtraindo o desconto.
 
-
-
-
-
-
-
-
-
-
-
-
-
   #print("--------------------------------------exe 10----------------------------------------")
-
 #16. Faça um programa que calcule o reajuste do salário de um
 #    funcionário. Para isso, o programa deverá ler o salário atual
 #    do funcionário e ler o percentual de reajuste. Ao final imprimir
 #    o valor do novo salário.
 
-
-
-
-
-
-
-
-
-
-
-
-
   #print("--------------------------------------exe 10----------------------------------------")
-
 #17. Faça um programa que calcule a conversão entre graus centígrados
 #    e Fahrenheit. Para isso, leia o valor em centígrados e calcule
 #    com base na fórmula a seguir. Após calcular o programa deve
 #    imprimir o resultado da conversão.
 #    F = (9 x C + 160) / 5
 
-
-
-
-
-
-
-
-
-
-
   #print("--------------------------------------exe 10----------------------------------------")
-
-
 #18. Faça um programa que calcule a quantidade de litros de combustível
 #    consumidos em uma viagem, sabendo-se que o carro tem autonomia de
 #    12 km por litro de combustível. O programa deverá ler o tempo
@@ -371,9 +146,7 @@
 #    Ao final, o programa deverá imprimir a distância percorrida e a
 #    quantidade de litros consumidos na viagem.
 
-
   #print("--------------------------------------exe 10----------------------------------------")
-
 #19. Faça um programa que calcule o valor de uma prestação em atraso.
 #    Para isso, o programa deve ler o valor da prestação vencida, a
 #    taxa periódica de juros e o período de atraso. Ao final, o
@@ -381,15 +154,7 @@
 #    de atraso, os juros que serão cobrados pelo período de atraso, o
 #    valor da prestação acrescido dos juros. Considere juros simples.
 
-
-
-
-
-
-
   #print("--------------------------------------exe 10----------------------------------------")
-
-
 #20. Faça um programa que efetue a apresentação do valor da conversão
 #    em real (R$) de um valor lido em dólar (US$). Para isso, será
 #    necessário também ler o valor da cotação do dólar.
@@ -405,7 +170,7 @@
 
 
 
-  #print("--------------------------------------exe 10----------------------------------------")
+  
 
 
 
