a2251edf9ed4a6b5c80d1dc8794b0f7311ebf46a
Lucas Ponce <150847602+LucasPonc@users.noreply.github.com>
2024-02-13 19:10:28 +0000

    3 exe

---
+++ curso.py
@@ -0,0 +1,23 @@
+#nome = input('Qual é o seu nome?')
+#print('olá prazer em conhecer você !',nome)
+#(----------------------------------------------------------)
+#dia = input('Que dia você nasceu ?')
+#print("\n")
+#mes = input('Qual mês você nasceu ?')
+#print("\n")
+#ano = input('Qual ano você nasceu ?')
+#print("\n")
+#print('você nasceu' ,dia ,mes,ano ,'correto!')
+#(-----------------------------------------------------------)
+#numero1 = int(input ('Digite numero1: '))
+#print("\n")
+#numero2 = int(input ('Digite numero2: '))
+#print("\n")
+#print('A soma  dos  numeros é: ' ,numero1 + numero2 )
+#print("\n")
+#(-------------------------------------------------------------)
+
+
+
+
+
