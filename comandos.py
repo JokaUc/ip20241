539006cb195be4a3105e56bbb4fc79fb3872061f
Lucas Ponce <150847602+LucasPonc@users.noreply.github.com>
2024-02-21 23:24:24 +0000

    Atualização

--- comandos.py
+++ comandos.py
@@ -1,32 +1,59 @@
 
-#procedimento para salvar os arquivos no github:
+# #procedimento para salvar os arquivos no github:
+# (------------------------------------------------)
+# #git add nome do arquivo.py
+# (------------------------------------------------)
+# #git commit -m "digitar texto para atualizacao"
+# (------------------------------------------------)
+# #git push
+# (-----------------------------------)
+# #clear comando para limpar o terminal
+# (------------------------------------------------------------------------)
+# #Ao digitar o comando python3 nomedoarquivo.py mostra o teste do arquivo 
+# (-------------------------------------------------------------------------)
+# #Variaves:
+# (----------------------------------------------------)
+# #esponenciação (1**2)
+# (----------------------------------------------------)
+# #divisao (1/2)
+# (----------------------------------------------------)
+# #inteiro da divisao (2//5) vai pegar só a parte inteira da divisão = 2
+# (----------------------------------------------------)
+# #resto da divisão (5%2) vai retornar 1 que o resto da divisão
+# (----------------------------------------------------)
+# #atribuição ou seja recebendo nome = a
+#  (---------------------------------------------------)
+# #type descobre o tipo da variavel(var)
+# (----------------------------------------------------)
+# #float numeros reais 2.5 ou 2,5
 
-#git add nome do arquivo.py
-#git commit -m "digitar texto para atualizacao"
-#git push
-#clear comando para limpar o terminal
-#Ao digitar o comando python3 nomedoarquivo.py mostra o teste do arquivo 
-#Variaves:
-#esponenciação (1**2)
-#divisao (1/2)
-#inteiro da divisao (2//5) vai pegar só a parte inteira da divisão = 2
-#resto da divisão (5%2) vai retornar 1 que o resto da divisão
-#atribuição ou seja recebendo nome = a 
-#type descobre o tipo da variavel(var)
-#float numeros reais 2.5 ou 2,5
-opcao = int(input('Questão a ser executada: '))
-match opcao:
-    case 1: q01()
-    case 2: q02()
-    case _: print('Opção Inválida!')
+# (----------------------------------------------------)
+# # opcao = int(input('Questão a ser executada: '))
+# # match opcao:
+# #     case 1: q01()
+# #     case 2: q02()
+# #     case _: print('Opção Inválida!')
+# (------------------------------------------------------)
+# nome = 'lucas'
 
+# if nome == ('lucas'):
+#     print('Sim é o lucas')
+# else: 
+#     print('Não é lucas')
+# (---------------------------)
+# #int - inteiro
+# (---------------------------)
+# #print - imprimir 
+# (---------------------------)
+# #print("\n") comando para quebrar uma linha.
+# (--------------------------------------------)
+# #{} para inserir uma variavel qualquer 
+# (--------------------------------------------)
+# #f e para formatar o texto inserido no projeto
+#(--------------------------------------------------)
 
+# if
 
+# elif sempre vai no meio dos dois comandos if e else
 
-#int - inteiro
-#print - imprimir 
-
-
-#print("\n") comando para quebrar uma linha.
-#{} para inserir uma variavel qaulquer 
-#f e para formatar o texto inserido no projeto
+# else
